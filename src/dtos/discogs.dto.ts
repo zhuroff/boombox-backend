fe88b5c0f569955c0f155b0b3725e74268166784
ezhuroff <ezhuroff@gmail.com>
2023-10-17 23:51:14 +0300

    discogs types refactoring

--- src/dtos/discogs.dto.ts
+++ src/dtos/discogs.dto.ts
@@ -4,28 +4,28 @@ export class DiscogsDTO {
   id: number
   country: string
   cover: string
-  format: string[]
+  releaseFormat: string[]
   genre: string[]
   style: string[]
   label: string[]
   masterApiUrl: string
   releaseApiUrl: string
   pageURL: string
-  title: string
-  year: string
+  releaseTitle: string
+  releaseYear: string
 
   constructor(item: DiscorsResponseItem) {
     this.id = item.id
     this.country = item.country
     this.cover = item.cover_image
-    this.format = item.format
+    this.releaseFormat = item.format
     this.genre = item.genre
     this.style = item.style
     this.label = item.label
     this.masterApiUrl = item.master_url
     this.releaseApiUrl = item.resource_url
     this.pageURL = `https://www.discogs.com${item.uri}`
-    this.title = item.title
-    this.year = item.year
+    this.releaseTitle = item.title
+    this.releaseYear = item.year
   }
 }
