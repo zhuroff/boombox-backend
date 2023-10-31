fcd3871e82aeb56ab7c49c3839376e807f27e846
ezhuroff <ezhuroff@gmail.com>
2023-11-01 00:05:50 +0300

    discogs service refactoring

--- src/services/discogs.services.ts
+++ src/services/discogs.services.ts
@@ -2,21 +2,31 @@ import { CloudLib } from '../lib/cloud.lib'
 import { Cloud } from '../'
 import { DiscogsPayload } from '../types/album.types'
 import { DiscogsDTO } from '../dtos/discogs.dto'
-import { PaginationDTO } from '../dtos/pagination.dto'
+// import { PaginationDTO } from '../dtos/pagination.dto'
 
 class DiscogsServices {
-  async getList({ artist, album, page }: DiscogsPayload) {
+  async getList({ artist, album, page }: DiscogsPayload, results: DiscogsDTO[] = []): Promise<DiscogsDTO[]> {
     const discogsUrl = String(
       'type=release' +
       '&artist=' + artist +
       '&release_title=' + album +
-      '&sort=year&sort_order=asc&per_page=100' +
+      '&sort=year&sort_order=asc&per_page=500' +
       '&page=' + page
     )
     const response = await Cloud.getDiscogsList(discogsUrl)
-    return {
-      pagination: new PaginationDTO(response.pagination),
-      data: response.results.map((el) => new DiscogsDTO(el))
+
+    results.push(...response.results.reduce<DiscogsDTO[]>((acc, next) => {
+      const releaseAlbum = next.title.slice(next.title.indexOf(' - ') + 3)?.trim()
+      if (releaseAlbum?.toLowerCase() === album.toLowerCase()) {
+        acc.push(new DiscogsDTO(next))
+      }
+      return acc
+    }, []))
+    
+    if (response.pagination.page < response.pagination.pages) {
+      return this.getList({ artist, album, page: response.pagination.page + 1 }, results)
+    } else {
+      return results
     }
   }
 
